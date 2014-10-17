b8606b76dddb5a070687e967084fa932e6a4e014
Oliver <obucaojit@gmail.com>
2014-10-16 23:39:43 -0700

    more trie work

--- trie/MyTrie.java
+++ trie/MyTrie.java
@@ -1,20 +1,34 @@
 package trie;
+
+import java.util.ArrayList;
+
 /**
  * {@code MyTrie} is a simple implementation of a trie <p>
  * No deletion of values in this current version 
+ * Character node values
  * <p>
  * @author Oliver B
  */
 
 public class MyTrie {
 	
-	TrieNode<Container> root;
+	TrieNode root;
 	Integer size;
 	Integer numContainers;
 	
-	void addValue(String key, Container data) {
-		// TODO
+	boolean addValue(String key, Container data) {
+		// need to find where to add the key
+		// 1) check if it exists
+		//    -if exists and is already isWord then false
+		//    -else make into isWord and set data
+		// 2) if not exist, then add the value. 
+		//    -for each intermediate node check if the
+		//       next char exists, if not create otherwise traverse
+		//    -create node, return true
+		           
+		
 			size++;
+			return true;
 	}
 	
 	Container getValue(String key) {
@@ -25,16 +39,29 @@ public class MyTrie {
 		return new Container();
 	}
 	
-	
-	class TrieNode<T>{
+	// Each TrieNode can have any number of children, different from a 
+	// binary tree. 
+	class TrieNode{
 		public boolean isWord;
-		public T data;
-		public TrieNode<T>[] children;
+		public char data;
+		public ArrayList<TrieNode> children;
 		public String value;
 		
-		void addChild() {
-			// TODO
+		// Other information stored in the trie node
+		public Container info;
+		
+		public TrieNode() {
+			isWord = false;			
+			children = new ArrayList<TrieNode>(1);
+			value = null;
 		}
+		
+		void addChild(char value) {
+			TrieNode child = new TrieNode();
+			child.data = value;
+			sortChildren();
+		}
+		
 		void sortChildren() {
 			// TODO
 		}
