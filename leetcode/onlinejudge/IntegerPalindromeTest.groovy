3c1aa99b7c938311cd2d72810d4cb76b5a5487cb
Oliver <obucaojit@gmail.com>
2014-10-29 19:22:10 -0700

    Junit test

---
+++ leetcode/onlinejudge/IntegerPalindromeTest.groovy
@@ -0,0 +1,19 @@
+package leetcode.onlinejudge
+
+/**
+ * Created by Oliver on 10/24/14.
+ */
+class IntegerPalindromeTest extends groovy.util.GroovyTestCase {
+    void setUp() {
+        super.setUp()
+
+    }
+
+    void tearDown() {
+
+    }
+
+    void testIsPalindrome() {
+        System.out.println("Testing running");
+    }
+}
